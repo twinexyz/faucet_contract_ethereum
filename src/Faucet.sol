a6e883183533661d92a123fa6fc518f1fd8c0139
Sarthak-5harma <sarthaak.sharma@gmail.com>
2025-09-01 15:29:27 +0545

    chore: Update readme

--- src/Faucet.sol
+++ src/Faucet.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";      // Import Ownable to call its constructor
+import "@openzeppelin/contracts/access/Ownable.sol";      
 import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
